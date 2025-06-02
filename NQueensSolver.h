8c2f0b14606b2b161bef556d3c22cf6e342cacf8
Amankr83 <142674725+Amankr83@users.noreply.github.com>
2025-06-03 00:30:31 +0530

    Update NQueensSolver.h

--- NQueensSolver.h
+++ NQueensSolver.h
@@ -8,12 +8,10 @@ using namespace std;
 class NQueensSolver {
 public:
     vector<vector<string>> solveNQueens(int n);
-    
+
 private:
-    void backtrack(int row, int n, vector<string>& board,
-                   vector<vector<string>>& solutions,
+    void backtrack(int row, int n, vector<string>& board, vector<vector<string>>& solutions,
                    vector<bool>& cols, vector<bool>& diag1, vector<bool>& diag2);
 };
 
-#endif
-
+#endif // NQUEENSSOLVER_H
